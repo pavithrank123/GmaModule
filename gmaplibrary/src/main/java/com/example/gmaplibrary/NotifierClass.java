6c3107eb3336ca60e43adc428db1d8e4564b9aeb
Pavithran <pavithran.k@ninjacart.com>
2019-05-28 10:42:04 +0530

    Made the class public

--- gmaplibrary/src/main/java/com/example/gmaplibrary/NotifierClass.java
+++ gmaplibrary/src/main/java/com/example/gmaplibrary/NotifierClass.java
@@ -3,9 +3,9 @@ package com.example.gmaplibrary;
 import android.content.Context;
 import android.widget.Toast;
 
-class NotifierClass {
+public class NotifierClass {
 
-    private void showNotification(Context context,String string)
+    public void showNotification(Context context,String string)
     {
         Toast.makeText(context,string,Toast.LENGTH_SHORT);
     }
